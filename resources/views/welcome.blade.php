6273922abb857df6ed57d047da9afe8aa1a0a658
Jacob Hall <jacobhall@Jacobs-MBP.localdomain>
2018-11-08 09:46:34 -0500

    Moves delete button on posts for symmetry

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,12 +22,21 @@
     @foreach ($posts as $post)
     <div class="card mt-3 mb-3" style="width: 18rem;" id="posted">
         <div class="card-body">
-          <p class="card-subtitle text-muted text-center">{{ $post->name }}</p>
-          <h5 class="card-title text-center" id="postcardtitle">{{ $post->title }}</h5>
-          <p class="card-text">{{$post->content}}</p>
-          <p class="edit-card"><a href="/posts/{{ $post->id }}/edit">Edit</a></p>
-          <p class="delete-card"><a href="/posts/{{ $post->id }}/delete">Delete</a></p>
-
+          <div class="row">
+            <div class="col">
+                <p class="card-subtitle text-muted text-center">{{ $post->name }}</p>
+                <h5 class="card-title text-center" id="postcardtitle">{{ $post->title }}</h5>
+                <p class="card-text">{{$post->content}}</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+                <p class="edit-card"><a href="/posts/{{ $post->id }}/edit">Edit</a></p>
+            </div>
+            <div class="col">
+              <p class="delete-card"><a href="/posts/{{ $post->id }}/delete">Delete</a></p>
+            </div>
+          </div>
         </div>
     </div>
     @endforeach
